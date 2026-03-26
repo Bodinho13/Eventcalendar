f4af5107bf8fe5c27a553f8c881f27189481c6a1
Marcus Bode <bodemarcus13@gmail.com>
2026-03-26 18:20:04 +0100

    Added deleting events from the Summary page and some visual changes on this page

--- src/components/EwEventCard.tsx
+++ src/components/EwEventCard.tsx
@@ -1,16 +1,34 @@
 import type { Event } from "../../shared/src/event";
 import { formatDateGerman } from "../utils/useSummary";
+import {useNavigate} from "react-router-dom";
 
 const EwEventCard = (props: any) => {
     const event: Event = props.event;
 
+    const navigate = useNavigate();
 
     return (
         <div className="eventCard">
             <span className="eventCard-title">{event.eventName}</span>
-            <div>Wann: {formatDateGerman(new Date(event.date))} {event.startTime}</div>
-            <div>Wo: {event.location}</div>
-            <div>Host: {event.host}</div>
+            <div style={{display: "flex", justifyContent: "space-between"}}>
+                <table className="cardInfo">
+                    <tbody>
+                        <tr>
+                            <td>Wann:</td>
+                            <td>{formatDateGerman(new Date(event.date))} {event.startTime}</td>
+                        </tr>
+                        <tr>
+                            <td>Wo:</td>
+                            <td>{event.location}</td>
+                        </tr>
+                        <tr>
+                            <td>Host:</td>
+                            <td>{event.host}</td>
+                        </tr>
+                    </tbody>
+                </table>
+                <span style={{fontSize: "40px"}} onClick={() => navigate(`/event/${event.id}`)}>{"〉"}</span>
+            </div>
         </div>
     )
 }
